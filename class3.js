66b5a8e4c54ab2127d466302fb87c766a906eabe
maryamlatif <161033266+maryamlatif@users.noreply.github.com>
2024-03-20 00:38:59 +0500

    Add files via upload
    In class 3rd I learnt about Addition (+),Multiplication(*),Subtraction(-),Exponent[**],Module(%)

---
+++ class3.js
@@ -0,0 +1,39 @@
+let fullName = "Maham latif";
+console.log(fullName);
+//its called singal line comment
+console.log(fullName);
+console.log(fullName);
+console.log(fullName);
+//Multi line comment  /* ye paraghrap ko  comment krny keliya hota ha oe iska opening or closing tag dono ha *\
+/* maham
+  25
+ umar
+   26
+ software Engineer*/
+ 
+//Add
+var Num2 = 3;
+var Num3 = 4;
+var total = Num2 + Num3;
+console.log(total);
+//sub
+var subtraction = 2;
+console.log(total - subtraction);
+//multiply
+var multiplyAnswer = 5 * 9;
+console.log(multiplyAnswer);
+//  division
+var totalapples = 20;
+//let bag:number=4;
+//let eachBag :number = totalapples / bag;
+//console.log(eachBag)
+//EXPONENTATION[**] Is use for 2*2*3*4*4*multiplication
+var apples = 5;
+var people = 5;
+var power = Math.pow(apples, 3);
+console.log(power);
+//MODULUS %
+var fruits = 7;
+var basket = 3;
+var reminder = fruits % basket;
+console.log(reminder);
---
+++ class3.ts
@@ -0,0 +1,53 @@
+let fullName = "Maham latif";
+console.log(fullName);
+
+//its called singal line comment
+
+//Multi line comment  /* ye paraghrap ko  comment krny keliya hota ha or iska opening or closing tag dono ha *\
+
+/* maham 
+                    25
+                   umar
+                     26
+                   software Engineer*/
+
+//Add
+
+let Num2: number = 3;
+let Num3: number = 4;
+let total = Num2 + Num3;
+console.log(total);
+
+//sub
+
+let subtraction: number = 2;
+console.log(total - subtraction);
+
+//multiply
+
+let multiplyAnswer: number = 5 * 9;
+console.log(multiplyAnswer);
+
+//  division
+
+let totalapples: number = 20;
+let bag:number=4;
+let eachBag :number = totalapples / bag;
+console.log(eachBag)
+
+//EXPONENTATION[**] Is use for 2*2*3*4*4*multiplication
+
+let apples: number = 5;
+
+let people: number = 5;
+
+let power = apples ** 3;
+
+console.log(power);
+
+//MODULUS %
+
+let fruits = 5;
+let basket = 2;
+let reminder = fruits % basket;
+console.log(reminder);
